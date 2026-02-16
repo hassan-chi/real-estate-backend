c1918fb62b5d054ae8c1d3d0bbeaf0544e09f6e3
hassan-chi <19276594@brookes.ac.uk>
2026-02-16 23:05:52 +0300

    feat: Add a database seeding command, health check endpoints, and configure Gunicorn for port binding.

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.http import HttpResponse
 from ninja import NinjaAPI
 from .api.api import router
 
+def health(request):
+    return HttpResponse("OK")
 
 mobile_api = NinjaAPI(
     title="API",
@@ -13,4 +16,6 @@ mobile_api.add_router("", router, tags=["Api"])
 
 urlpatterns = [
     path("api/", mobile_api.urls),
+    path("health/", health),
+    path("", health),  # Also efficient for default health checks
 ]
