8008dc58138325fbba9a129a1e4a9248d2c55d2b
hassan-chi <19276594@brookes.ac.uk>
2026-02-16 22:12:21 +0300

    fix some issues

--- backend/settings.py
+++ backend/settings.py
@@ -16,6 +16,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = (os.getenv("DEBUG", "True").strip().lower() in ("1", "true", "yes", "y", "on"))
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["https://*.railway.app"]
 
 # Application definition
 
@@ -36,6 +37,7 @@ INSTALLED_APPS = [
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -110,6 +112,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 AUTH_USER_MODEL = 'core.CustomUser'
 
